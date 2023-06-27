454a220c8e2022a01e09b527e89f8802ba865b00
carmennovilllo <81377181+carmennovilllo@users.noreply.github.com>
2023-06-27 08:41:58 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  server_url: "https://dummyjson.com/products", //cuál tengo que poner
+  num_items: 30,  
+  use_server: false,
+  loading_timeout_ms: 300
+}
+
+export default CONFIG;
